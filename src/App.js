2dd7b525bbd2d83f2684a7aa6cea483c419412ee
3omda8 <mohameddawood664@gmail.com>
2024-09-13 15:46:28 +0200

    6th

--- src/App.js
+++ src/App.js
@@ -15,39 +15,39 @@ function App() {
 
       <div className="second">
         <div>
-          <h1 style={{ fontSize: "3.6rem" }}> Moustafa Emad Dawood</h1>
+          <h1 style={{ fontSize: "2.5rem" }}> Moustafa Emad Dawood</h1>
         </div>
         <div>
-          <h2 style={{ fontSize: "2.6rem" }}>Mobile : 01096688747</h2>
+          <h2 style={{ fontSize: "1.5rem" }}>Mobile : 01096688747</h2>
         </div>
         <div>
           <a href="https://wa.me/+201096688747">
             <FontAwesomeIcon
               icon={faWhatsapp}
-              style={{ fontSize: "6rem", margin: "0.2rem" }}
+              style={{ fontSize: "4rem", margin: "0.4rem" }}
             />
           </a>
 
           <a href="https://www.facebook.com/profile.php?id=100004980244806">
             <FontAwesomeIcon
               icon={faFacebook}
-              style={{ fontSize: "6rem", margin: "0.2rem" }}
+              style={{ fontSize: "4rem", margin: "0.4rem" }}
             />
           </a>
           <a href="https://www.instagram.com/moustafa_3mad99">
             <FontAwesomeIcon
               icon={faSquareInstagram}
-              style={{ fontSize: "6rem", margin: "0.2rem" }}
+              style={{ fontSize: "4rem", margin: "0.4rem" }}
             />
           </a>
           <a href="https://www.google.com.eg">
             <FontAwesomeIcon
               icon={faSnapchatGhost}
-              style={{ fontSize: "6rem", margin: "0.2rem" }}
+              style={{ fontSize: "4rem", margin: "0.4rem" }}
             />
           </a>
 
-          <p style={{ marginTop: "6rem", fontSize: "1.2rem" }}>
+          <p style={{ marginTop: "4rem", fontSize: "1.2rem" }}>
             @ created by Mohamed Dawood
           </p>
         </div>
